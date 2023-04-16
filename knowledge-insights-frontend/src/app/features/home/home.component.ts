5dd94dd7b38ad5c8d78a7ff2dbd51bc41f9f5567
mikesimmonds <mike@mikesimmonds.io>
2023-04-16 01:42:10 +0100

    fix navigation now that /questions is not used. Add message if user not authed

--- knowledge-insights-frontend/src/app/features/home/home.component.ts
+++ knowledge-insights-frontend/src/app/features/home/home.component.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { map } from 'rxjs';
+import { Observable, catchError, map } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
 interface ChatMessage {
@@ -27,14 +27,24 @@ export class HomeComponent {
     if (this.userInput.trim()) {
       this.chatMessages.push({ answer: this.userInput, sender: 'user' });
       this.askQuestion(this.userInput).subscribe((response) => {
-        this.chatMessages.push({ answer: response, sender: 'gpt', question: this.userInput.trim() });
+        this.chatMessages.push({ answer: response.text, sender: 'gpt', question: this.userInput.trim() });
       });
       this.userInput = '';
     }
   }
 
-  askQuestion(questionText: string) {
-    return this.httpClient.post<{text:string}>(`${environment.api.serverUrl}/api/questions/ask`, { questionText, chatHistory: this.chatMessages.map(msg => msg.answer + msg.question) }).pipe(map(response => response.text));
+  askQuestion(questionText: string): Observable<any> {
+    return this.httpClient.post<{ text: string }>(`${environment.api.serverUrl}/api/questions/ask`, { questionText, chatHistory: this.chatMessages.map(msg => msg.answer + msg.question) })
+      .pipe(
+        map(response => response.text ),
+        catchError(error => { 
+          // if error is 401 or 403 add a message to the chatMessages array
+          if (error.status === 401 || error.status === 403) {
+            this.chatMessages.push({ answer: 'You are not authorized to ask questions', sender: 'gpt' });
+          }
+          return error;
+        })
+      );
   }
 
 }
